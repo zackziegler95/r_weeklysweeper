441ec5471bcab14cf6fe7eaa858fc11a6b32aa84
django rineheart <dontemailme@google.com>
2012-01-19 22:08:10 -0500

    added commentlink parsing and link score from comment page discovery

--- r_weeklysweeper/redditparse.py
+++ r_weeklysweeper/redditparse.py
@@ -1,92 +1,79 @@
 import urllib
 import HTMLParser
 import re
+import sys
 
 from HTMLParser import HTMLParser
 
 submissionreg = r'thing\s\S+\seven|odd'
-sublist = []
-tabd = ['', '	', '		', '			', '				','					']
-subcnt = 0
 
 #these methods get called for every element on page
 class SubmissionHTMLParser(HTMLParser):
   def __init__(self):
     HTMLParser.__init__(self)
     self.withinlinkdiv = -1
-    self.foundvotecount = False
-    self.foundtitle = False
-    
+    self.current_value = 0 # 0 = nothing, 1 = votes, 2 = title this is used to
+                           # link the data and the tag
+    self.tempdata = [0, '', '', ''] # votes, title, link temporary, before it is put
+                                # into the submission
+    self.sublist = [] # sublist for the submissions
 
   def handle_starttag(self, tag, attrs):
     if self.withinlinkdiv > -1:
-      #data processing goes here
       self.withinlinkdiv += 1
-      print tabd[self.withinlinkdiv], tag, attrs
-
-      #selects the total votes
       if self.withinlinkdiv == 2 and tag == 'div' and attrs[0][1] == 'score unvoted':
-        self.foundvotecount = True
-
-      if self.withinlinkdiv == 3 and tag == 'a' and attrs[0][1] == 'title ':
-	reusedsub.link = attrs[1][0]
-	self.foundtitle = True
+        self.current_value = 1 # Indicates that the data is the vote
+      if self.withinlinkdiv == 3 and tag == 'a' and attrs[0][0] == 'class':
+        self.current_value = 2 # Indictaes that the data is the title
+        self.tempdata[2] = attrs[1][1] # The link is in the tag, so we take
+                                       # that directly
+      # this finds the link to the comment page of the article
+      if self.withinlinkdiv == 4 and tag == 'a' and attrs[0][1] == 'comments':
+        self.current_value = 3
+	self.tempdata[3] = attrs[1][1]
 
     # selects what I think are the divs that represent submissions and checks to see if regex is not none
     if tag == 'div' and len(attrs) == 3 and len(attrs[0]) == 2 and re.search(submissionreg, str(attrs[0][1])):
       self.withinlinkdiv = 0
-      reusedsub = Submission()
-      print '---------new top-----------'
-
 
   def handle_endtag(self, tag):
-    if self.withinlinkdiv == 0:
-      sublist.append(reusedsub)
     if self.withinlinkdiv > -1:
       self.withinlinkdiv -= 1
-
   def handle_data(self, data):
-    #found vote count div... so reset the boolean and write the data
-    if self.foundvotecount:
-      self.foundvotecount = False
-      reusedsub.votes = data
-      reusedsub.print_out()
-    #found title data so reset and write!  
-    if self.foundtitle:
-      self.foundtitle = False
-      reusedsub.title = data
-      reusedsub.print_out()
-      #todo fix the write
-    if self.withinlinkdiv > -1:
-      print tabd[self.withinlinkdiv], data
+    if self.current_value == 1: # If the data is the vote
+      self.tempdata[0] = data
+      self.current_value = 0 # Reset the data indicator
+      
+    if self.current_value == 2: # If the data is the title
+      self.tempdata[1] = data
+      self.current_value = 0 # This is read last, so at this point we can create
+                             # the Submission object
+      self.sublist.append(Submission(self.tempdata[0], self.tempdata[1], self.tempdata[2], self.tempdata[3]))
 
 class Submission():
-  def __init__(self, votes=0, link='', title=''):
+  def __init__(self, votes=0, title='', link='', commentlink=''):
     self.votes = votes
-    self.link = link
     self.title = title
+    self.link = link
+    self.commentlink = commentlink
   def print_out(self):
     print '----Submission----'
     print self.title
     print self.link
+    print 'comment link = ' + self.commentlink
     print 'votes = ' + str(self.votes)
-    print '------------------'
-  def clear(self):
-    self.votes = 0
-    self.link = ''
-    self.title = ''
-
-reusedsub = Submission()
+    print '------------------\n'
 
 
 def main():
-  f = urllib.urlopen('http://www.reddit.com/r/python')
+  args = sys.argv[1:]
+  f = urllib.urlopen(args[0])
   text = f.read()
   parser = SubmissionHTMLParser()
   parser.feed(text)
-  #for s in sublist:
-   # s.print_out()
-
+  for s in parser.sublist:
+    s.print_out()
+  import commentparse
 
 if __name__ == '__main__':
   main()
